3c8f8fa7fda561e5edefe886045b6e413bddbea7
Sai Venkata Manieswar Namburi <saimani9381787332@gmail.com>
2025-07-26 17:40:08 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,16 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://task-curd-mern-stack-rn9n.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(cors({
   origin: 'https://task-curd-mern-stack-rn9n.vercel.app',
