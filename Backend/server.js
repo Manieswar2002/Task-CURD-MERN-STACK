bd5907d4c6b3c08e083f4e7cdca8430500dd6caa
Sai Venkata Manieswar Namburi <saimani9381787332@gmail.com>
2025-07-26 17:30:06 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(cors({
-  origin: 'https://task-curd-mern-stack-rn9n.vercel.app/',
+  origin: 'https://task-curd-mern-stack-rn9n.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true 
 }));
